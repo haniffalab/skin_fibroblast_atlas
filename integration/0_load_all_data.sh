6f6ee56b416627148e1064c081bfd5da94e984a1
Lloyd Steele <ls34@sanger.ac.uk>
2024-12-17 16:19:47 +0000

    Add integration scripts and notebooks

---
+++ integration/0_load_all_data.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# activate conda environment
+source ~/.bashrc 
+source /software/cellgen/team298/ls34/milopy/bin/activate
+python3 /nfs/users/nfs_l/ls34/fibroblast_atlas/0_load_all_data.py
+
+
